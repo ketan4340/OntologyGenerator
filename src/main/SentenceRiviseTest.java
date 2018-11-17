08b7de0bec6fbdf12f224f798e923c289d60e9b3
tanaken915 <ken_tanabe915@outlook.jp>
2018-11-18 00:11:11 +0900

    added main class for sentence test.

---
+++ src/main/SentenceRiviseTest.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import grammar.naturalLanguage.NaturalLanguage;
+import grammar.sentence.Sentence;
+import modules.syntacticParse.SyntacticParser;
+
+public class SentenceRiviseTest {
+
+	public static void main(String[] args) {
+		String textFile_str = "resource/input/test/single.txt";
+		List<String> texts;
+		try {
+			texts = Files.readAllLines(Paths.get(textFile_str));
+		} catch (IOException e) {
+			e.printStackTrace();
+			texts = new ArrayList<>(Arrays.asList("ジェネレータはファイルからテキストを読み込めませんでした。"));
+		}
+		List<NaturalLanguage> naturalLanguages = NaturalLanguage.toNaturalLanguageList(texts);
+		
+		Path propPath = Paths.get("resource/prop/CaboCha-property.xml");
+		List<Sentence> sentenceList = new SyntacticParser(propPath).parseSentences(naturalLanguages);
+
+		
+		
+	}
+}
